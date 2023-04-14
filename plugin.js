943bd92da4a047d6f3f0925c883d7e3591109a3b
Harsh Trivedi <60310287+Harsh-Trivedi@users.noreply.github.com>
2023-04-14 19:00:04 +0530

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -25,7 +25,7 @@
 
 	$.fn[pluginName] = function(options) {
 		return this.each(function() {
-            //prevent to reinitialize the plugin
+            		//prevent to reinitialize the plugin
 			var pluginInstance = $.data(this, pluginName);
 			if (!pluginInstance) {
 				pluginInstance = new Plugin(this, options);
