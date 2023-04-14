036c4b81d0d567894daa6db60c55da076f5247a5
Harsh Trivedi <60310287+Harsh-Trivedi@users.noreply.github.com>
2023-04-14 18:37:00 +0530

    Initial Upload

---
+++ plugin.js
@@ -0,0 +1,36 @@
+(function($, window, document, undefined) {
+	"use strict";
+
+	var pluginName = "defaultPluginName",
+		defaults = {
+			text: "jQuery Boilerplate"
+		};
+
+	function Plugin(element, options) {
+		this.element = element;
+		this.settings = $.extend({}, defaults, options);        
+		this.init();
+	}
+
+	$.extend(Plugin.prototype, {
+		init: function() {
+			this.setText(this.settings.text);
+			$(this.element).data(pluginName, this);
+		},
+		setText: function(text) {
+			$(this.element).text(text);
+            this.settings.text = text;
+		}
+	});
+
+	$.fn[pluginName] = function(options) {
+		return this.each(function() {
+            //prevent to reinitialize the plugin
+			var pluginInstance = $.data(this, pluginName);
+			if (!pluginInstance) {
+				pluginInstance = new Plugin(this, options);
+			}
+			$(this).data(pluginName, pluginInstance);
+		});
+	};
+})(jQuery, window, document);
